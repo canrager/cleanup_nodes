37c6c9e93a53ebe8e5bb6bd44d85597efec97327
canrager <can.rager@posteo.de>
2023-06-22 18:05:33 +0000

    added node-resid projection

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import einops
 torch.set_grad_enabled(False)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-from transformer_lens import HookedTransformer
+from transformer_lens import HookedTransformer, ActivationCache
 from pathlib import Path
 import pickle
 import plotly.express as px
@@ -28,21 +28,21 @@ from datasets import load_dataset
 
 def get_prompts_list(dataset_name: str, n_prompts: int, shuffle_buffer_size: int, shuffle_seed: int):
     print(f"Loading {n_prompts} prompts from {dataset_name}...")
-    file_name = f"{dataset_name}-{n_prompts}-seed{shuffle_seed}-buffer{shuffle_buffer_size}.pkl"
-    file_path = "./data" / Path(file_name) # Change based on user
-    if file_path.exists():
-        print("Using pickled prompts...")
-        with open(file_path, "rb") as f:
-            return pickle.load(f)
-    print("Downloading from HuggingFace...")
+    # file_name = f"{dataset_name}-{n_prompts}-seed{shuffle_seed}-buffer{shuffle_buffer_size}.pkl"
+    # file_path = "./data" / Path(file_name) # Change based on user
+    # if file_path.exists():
+    #     print("Using pickled prompts...")
+    #     with open(file_path, "rb") as f:
+    #         return pickle.load(f)
+    # print("Downloading from HuggingFace...")
     prompts_list = []
     ds_unshuffled = load_dataset(f"NeelNanda/{dataset_name}", streaming=True, split="train")
     ds = ds_unshuffled.shuffle(buffer_size=shuffle_buffer_size, seed=shuffle_seed)
     ds_iter = iter(ds)
     for _ in trange(n_prompts):
         prompts_list.append(next(ds_iter)["tokens"])
-    with open(file_path, "wb") as f:
-        pickle.dump(prompts_list, f)
+    # with open(file_path, "wb") as f:
+    #     pickle.dump(prompts_list, f)
     return prompts_list
 
 # %% Dataset preprocessing
@@ -125,14 +125,40 @@ print("".join(model.to_str_tokens(prompts_t[10])))
 # %% Model inspection
 
 logits, activation_cache = model.run_with_cache(prompts_t[10])
-print(logits.shape)
+# print(logits.shape)
 
-print("".join(model.to_str_tokens(prompts_t[10])))
-print("prediction:", model.to_str_tokens(logits.argmax(dim=-1)[0, -1]))
+# print("".join(model.to_str_tokens(prompts_t[10])))
+# print("prediction:", model.to_str_tokens(logits.argmax(dim=-1)[0, -1]))
+
+
+neuron_idx = 0
+layer = 0
+
+def get_neuron_output(
+    cache: ActivationCache, layer_idx: int, neuron_idx: int
+) -> Float[Tensor, "batch pos dmodel"]:
+    neuron_activation = cache["mlp_post", layer_idx][:, :, neuron_idx]
+    neuron_wout = model.W_out[layer_idx, neuron_idx]
+    mlp_bias = model.b_out[layer_idx] / model.cfg.d_mlp         # TODO design choice, discuss with Jett
+
+    # print(f"{neuron_activation.shape=}")
+    # print(f"{neuron_wout.shape=}")
+    # print(f"{mlp_bias.shape=}")
+
+    neuron_out = neuron_activation.unsqueeze(dim=-1) * neuron_wout + mlp_bias
+    # print(neuron_activation.shape)
+    return neuron_out
+
+
+custom_mlp_out = sum(
+    [get_neuron_output(activation_cache, layer, neuron_idx) for neuron_idx in range(model.cfg.d_mlp)]
+)
+
+# torch.isclose(custom_mlp_out, activation_cache["mlp_out", layer], atol= 1e-5).all()
 
 # %% Get pattern with transformerlens
 def get_node_outputs_and_resid(name: str) -> bool:
-    hook_names = ["result", "mlp_out", "resid_mid", "resid_post"]
+    hook_names = ["result", "post", "resid_mid", "resid_post"]
     return any(hook_name in name for hook_name in hook_names)
 
 logits, activation_cache = model.run_with_cache(
@@ -151,15 +177,25 @@ def projection(
     dot_prod = einops.einsum(writer_out, cleanup_out, "batch pos dmodel, batch pos dmodel -> batch pos")
     return dot_prod / norm_writer_out
 
+def cos_similarity(
+    writer_out: Float[Tensor, 'batch pos dmodel'], 
+    cleanup_out: Float[Tensor, 'batch pos dmodel']
+) -> Float[Tensor, 'batch pos']:
+    """Compute the projection from the cleanup output vector to the writer output direction"""
+    norm_writer_out = torch.norm(writer_out)
+    norm_cleaner_out = torch.norm(cleanup_out)
+    dot_prod = einops.einsum(writer_out, cleanup_out, "batch pos dmodel, batch pos dmodel -> batch pos")
+    return dot_prod / (norm_writer_out * norm_cleaner_out)
+
 # prompt 0, 10th position
 ## all attentions
-attn_hook_names = [name for name in activation_cache.keys() if "result" in name]
-attn_activations = [activation_cache[name] for name in attn_hook_names]
-attn_activations = einops.rearrange(attn_activations, "layer batch pos head dmodel -> batch pos (layer head) dmodel")
+# attn_hook_names = [name for name in activation_cache.keys() if "result" in name]
+# attn_activations = [activation_cache[name] for name in attn_hook_names]
+# attn_activations = einops.rearrange(attn_activations, "layer batch pos head dmodel -> batch pos (layer head) dmodel")
 
-mlp_hook_names = [name for name in activation_cache.keys() if "mlp_out" in name]
-mlp_activations = [activation_cache[name] for name in mlp_hook_names]
-mlp_activations = einops.rearrange(mlp_activations, "layer batch pos head dmodel -> batch pos (layer head) dmodel")
+# mlp_hook_names = [name for name in activation_cache.keys() if "mlp_out" in name]
+# mlp_activations = [activation_cache[name] for name in mlp_hook_names]
+# mlp_activations = einops.rearrange(mlp_activations, "layer batch pos head dmodel -> batch pos (layer head) dmodel")
 
 
 # for attn_layer
@@ -168,17 +204,17 @@ mlp_activations = einops.rearrange(mlp_activations, "layer batch pos head dmodel
 
 
 # %%
-
 def calc_node_node_projection(
-    tokens: List[int]
+    tokens: List[int],
+    proj_func: Callable,
 ) -> Float[Tensor, "head neuron prompt pos"]:
     all_heads = [
         (l, h) for l in range(model.cfg.n_layers) for h in range(model.cfg.n_heads)
-    ]
+    ] # [head0.0, head0.1, ..., head2.7]
 
     all_neurons = [
         (l, h) for l in range(model.cfg.n_layers) for h in range(model.cfg.d_mlp)
-    ]
+    ] # [neuron0.0, neuron0.1, ..., neuron2.2047]
 
     n_total_heads = len(all_heads)
     n_total_neurons = len(all_neurons)
@@ -200,15 +236,74 @@ def calc_node_node_projection(
         for w, (lw, hw) in enumerate(all_heads):
             for c, (lc, hc) in enumerate(all_neurons):
                 writer_out = cache['result', lw][:, :, hw, :]
-                cleaner_out = cache['mlp_out', lc][:, :, hc, :]
-                projection_matrix[w, c, pi:pi+10, :] = projection(
+                cleaner_out = get_neuron_output(cache, lc, hc)
+                projection_matrix[w, c, pi:pi+10, :] = proj_func(
                     writer_out, cleaner_out
                 )
 
     return projection_matrix
 # %%
 
-calc_node_node_projection(prompts_t)
+node_node_projections = calc_node_node_projection(prompts_t[:10], proj_func=projection)
+# %%
+
+def calc_node_resid_projection(
+    tokens: List[int],
+    proj_func: Callable,
+) -> Float[Tensor, "head resid prompt pos"]:
+    all_heads = [
+        (l, h) for l in range(model.cfg.n_layers) for h in range(model.cfg.n_heads)
+    ] # [head0.0, head0.1, ..., head2.7]
+
+    all_resids = [
+        (l, act) for l in range(model.cfg.n_layers) for act in ['resid_mid', 'resid_post']
+    ] # [resid_mid0, resid_post0, resid_mid1, ... ,resid_post2]
+
+    n_total_heads = len(all_heads)
+    n_total_resids = len(all_resids)
+    n_prompts = len(tokens)
+    n_pos = len(tokens[0])
+    
+    projection_matrix = torch.zeros(
+        n_total_heads,
+        n_total_resids,
+        n_prompts,
+        n_pos,
+    )
+
+    for pi in trange(0, len(tokens), 10):
+        _, cache = model.run_with_cache(
+            tokens[pi : pi + 10], names_filter=get_node_outputs_and_resid
+        )
+
+        for w, (lw, hw) in enumerate(all_heads):
+            for resid_idx, (resid_layer, resid_name) in enumerate(all_resids):
+                writer_out = cache['result', lw][:, :, hw, :]
+                resid_vec = cache[resid_name, resid_layer]
+                projection_matrix[w, resid_idx, pi:pi+10, :] = proj_func(
+                    writer_out, resid_vec
+                )
+
+    return projection_matrix
+
+# %%
+node_resid_projection_matrix = calc_node_resid_projection(prompts_t, cos_similarity)
+
+all_heads = [
+    (l, h) for l in range(model.cfg.n_layers) for h in range(model.cfg.n_heads)
+] # [head0.0, head0.1, ..., head2.7]
+
+all_resids = [
+    (l, act) for l in range(model.cfg.n_layers) for act in ['resid_mid', 'resid_post']
+] # [resid_mid0, resid_post0, resid_mid1, ... ,resid_post2]
 
 
+px.imshow(
+    node_resid_projection_matrix.flatten(start_dim=-2).min(dim=-1).values,
+    color_continuous_midpoint=0,
+    color_continuous_scale='RdBu',
+    y=[f"Head {layer}.{head}" for layer, head in all_heads],
+    x=[f"Resid {layer}.{resid}" for layer, resid in all_resids],
+    title="Node - Resid Projection"
+)
 # %%
