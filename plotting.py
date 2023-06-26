8de6547b017e85a1576b4117330678367d850586
canrager <can.rager@posteo.de>
2023-06-26 14:39:00 +0000

    plot projection value over position

--- plotting.py
+++ plotting.py
@@ -368,6 +368,18 @@ def single_head_full_resid_projection(
 def ntensor_to_long(tensor: Union[Tensor, np.array]) -> pd.DataFrame:
     """
     Converts an n-dimensional tensor to a long format dataframe.
+    Dimension numbering in long format from outer to inner dimension
+
+    Example:
+    tensor.shape = [batch pos dmodel]
+    df = ntensor_to_long(tensor)
+    df.columns
+    >>> [values, dim0, dim1, dim2]
+    where dims are corresponding to:
+    dim0: batch
+    dim1: pos
+    dim2: dmodel
+
     """
     df = pd.DataFrame()
     df["values"] = tensor.cpu().numpy().flatten()
@@ -410,3 +422,44 @@ if __name__ == "__main__":
 
     fig = get_fig_head_to_selected_mlp_neuron(projections, k, neuron_names, n_layers)
     fig.show()
+
+def line_with_river(error_y_mode=None, **kwargs):
+    """Extension of `plotly.express.line` to use error bands."""
+    ERROR_MODES = {'bar','band','bars','bands',None}
+    if error_y_mode not in ERROR_MODES:
+        raise ValueError(f"'error_y_mode' must be one of {ERROR_MODES}, received {repr(error_y_mode)}.")
+    if error_y_mode in {'bar','bars',None}:
+        fig = px.line(**kwargs)
+    elif error_y_mode in {'band','bands'}:
+        if 'error_y' not in kwargs:
+            raise ValueError(f"If you provide argument 'error_y_mode' you must also provide 'error_y'.")
+        figure_with_error_bars = px.line(**kwargs)
+        fig = px.line(**{arg: val for arg,val in kwargs.items() if arg != 'error_y'})
+        for data in figure_with_error_bars.data:
+            x = list(data['x'])
+            y_upper = list(data['y'] + data['error_y']['array'])
+            y_lower = list(data['y'] - data['error_y']['array'] if data['error_y']['arrayminus'] is None else data['y'] - data['error_y']['arrayminus'])
+            color = f"rgba({tuple(int(data['line']['color'].lstrip('#')[i:i+2], 16) for i in (0, 2, 4))},.3)".replace('((','(').replace('),',',').replace(' ','')
+            fig.add_trace(
+                go.Scatter(
+                    x = x+x[::-1],
+                    y = y_upper+y_lower[::-1],
+                    fill = 'toself',
+                    fillcolor = color,
+                    line = dict(
+                        color = 'rgba(255,255,255,0)'
+                    ),
+                    hoverinfo = "skip",
+                    showlegend = False,
+                    legendgroup = data['legendgroup'],
+                    xaxis = data['xaxis'],
+                    yaxis = data['yaxis'],
+                )
+            )
+        # Reorder data as said here: https://stackoverflow.com/a/66854398/8849755
+        reordered_data = []
+        for i in range(int(len(fig.data)/2)):
+            reordered_data.append(fig.data[i+int(len(fig.data)/2)])
+            reordered_data.append(fig.data[i])
+        fig.data = tuple(reordered_data)
+    return fig
